a37408358d5cbe6cbaf70839ab058cfc879997a1
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2021-06-24 23:34:41 -0500

    Added some object features

--- Objects/basic.js
+++ Objects/basic.js
@@ -50,3 +50,27 @@ const user = { // Este principio de destruir objetos también aplica para object
 
 const { johnDoe: { age: userAge, email: userEmail }} = user;
 console.log("John Dow's age is " + userAge + " and Email is " + userEmail);
+
+// Cuando creamos un objeto con una función, podemos usar esta sintaxis para facilitar el proceso: 
+console.log("\n > Objecto creado con sintaxis sencilla: ");
+const [nameE, ageE, genderE] = ["Daniman", 23, "Masculino"]; // Usamos array destructuring
+
+const createPerson = (name, age, gender) => ({ name, age, gender });
+
+console.log(createPerson(nameE, ageE, genderE));
+
+// -------------------------------------------------
+console.log("\n > Funciones dentro de objetos:");
+
+const bicycle = {
+    gear: 2,
+    brand: "Coca-cola",
+    setGear(newGear) { // No es necesario ponerle la palabra clave
+        this.gear = newGear;
+    },
+    setBrand(newBrand) {this.brand = newBrand}
+};
+
+bicycle.setGear(4563);
+bicycle.setBrand("Toyota");
+console.log("The bicycle brand is " + bicycle.brand + " and the gear is " + bicycle.gear);
