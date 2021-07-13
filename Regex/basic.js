e040ab8afd10c98783afcf1622e1f677f96427d2
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2021-07-13 18:49:43 -0500

    Added a regex feature

--- Regex/basic.js
+++ Regex/basic.js
@@ -2,3 +2,8 @@ let myString = "freeCodeCamp";
 let myString2 = "freeCodeCAMP";
 let fccRegex = /freecodecamp/i; // El i al final de la expresión se llama flag y se usa para ignorar la 
 console.log("El resultado de ambos strings con la expresión 'no casa sensitive'", fccRegex.test(myString), fccRegex.test(myString2));
+
+let testStr = "Repeat, repeat, REPEAT";
+let ourRegex = /Repeat/gi; // La flag 'g' es de Global y la i para case insesitive
+let result = testStr.match(ourRegex);
+console.log("Al usar match (y mientras la expresión regular tenga la g al final) obtienes esto:", result);
