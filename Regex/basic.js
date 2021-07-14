a7d56d120fed0dab1589256110e2cfe767b870e8
Daniel-dadu <67434516+Daniel-dadu@users.noreply.github.com>
2021-07-14 17:25:18 -0500

    Added more Regex features

--- Regex/basic.js
+++ Regex/basic.js
@@ -6,4 +6,16 @@ console.log("El resultado de ambos strings con la expresión 'no casa sensitive'
 let testStr = "Repeat, repeat, REPEAT";
 let ourRegex = /Repeat/gi; // La flag 'g' es de Global y la i para case insesitive
 let result = testStr.match(ourRegex);
-console.log("Al usar match (y mientras la expresión regular tenga la g al final) obtienes esto:", result);
+console.log("\nAl usar match (y mientras la expresión regular tenga la g al final) obtienes esto:", result);
+
+let text = "<h1>Winter is coming</h1>";
+let myRegex = /<.*?>/; // Change this line
+let myOtherRegex = /<.*>/;
+console.log("\nLa primera expresión con el '?' (lazy matching) regresa esto: ", text.match(myRegex)[0]); 
+console.log("La segunda expresión solo regresa esto: ", text.match(myOtherRegex)[0]); 
+
+let firstString = "Ricky is first and can be found.";
+let notFirst = "You can't find Ricky now.";
+let firstRegex = /^Ricky/;
+console.log("\nAl poner el símbolo '^' al principio de una expresión y fuera de los corchetes, busca la expresión al principio del string:");
+console.log("En el primer string se obtiene un", firstRegex.test(firstString), ", mientras que en el segundo nos regresa un", firstRegex.test(notFirst));
